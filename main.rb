1aa88dea1bc1f14ccfed202c0d9546434612fe18
Mohsin Jamshaid <mohsin.jamshaid777@gmail.com>
2022-04-28 15:43:52 +0500

    set main.rb as an entry point to trigger scheduler code

---
+++ main.rb
@@ -0,0 +1,7 @@
+require_relative 'meeting_scheduler'
+require_relative 'helper'
+
+# Entry point to schedule meetings
+MeetingScheduler.new(meetings1).schedule
+MeetingScheduler.new(meetings2).schedule
+MeetingScheduler.new(meetings3).schedule
