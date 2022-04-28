c3ad90511766b1ab3747cd51481da55e592aed75
Mohsin Jamshaid <mohsin.jamshaid777@gmail.com>
2022-04-28 15:38:45 +0500

    Moved input into helpers.rb

---
+++ helper.rb
@@ -0,0 +1,26 @@
+def meetings1
+  [
+    { name: "Meeting 1", duration: 3, type: :onsite },
+    { name: "Meeting 2", duration: 2, type: :offsite },
+    { name: "Meeting 3", duration: 1, type: :offsite },
+    { name: "Meeting 4", duration: 0.5, type: :onsite }
+  ]
+end
+
+
+def meetings2
+  [
+    { name: "Meeting 1", duration: 1.5, type: :onsite },
+    { name: "Meeting 2", duration: 2, type: :offsite },
+    { name: "Meeting 3", duration: 1, type: :onsite },
+    { name: "Meeting 4", duration: 1, type: :offsite },
+    { name: "Meeting 5", duration: 1, type: :offsite },
+  ]
+end
+
+def meetings3
+  [
+    { name: "Meeting 1", duration: 4, type: :offsite },
+    { name: "Meeting 2", duration: 4, type: :offsite }
+  ]
+end
